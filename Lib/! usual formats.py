f97d7df44655ede5c99da5f3554ed56cc701aa37
Eithea <oguraff94@gmail.com>
2022-09-02 23:37:51 +0900

    폴더 정리

--- Lib/! usual formats.py
+++ Lib/! usual formats.py
@@ -1,4 +1,4 @@
-# base
+# usual base
 import sys
 input = sys.stdin.readline
 
@@ -6,30 +6,31 @@ testcase = int(input())
 for t in range(testcase) : 
     break
 
-
-# DFS
-import sys
-input = sys.stdin.readline
-sys.setrecursionlimit(10**8)
-# BFS
-import sys
-input = sys.stdin.readline
-from collections import deque
-# heap loop
-import sys
-input = sys.stdin.readline
-from heapq import heappush, heappop
-
-# defaultdict
-import sys
-input = sys.stdin.readline
-from collections import defaultdict
-V = defaultdict(int)
-
-# input
+# usual input
 n = int(input())
 n, m = map(int, input().split())
 l = list(map(int, input().split()))
 for i in range(n) : 
     l.append(list(input().split()))
 
+
+# for DFS
+import sys
+input = sys.stdin.readline
+sys.setrecursionlimit(10**5)
+
+# for BFS
+import sys
+input = sys.stdin.readline
+from collections import deque
+
+# for DIJK
+import sys
+input = sys.stdin.readline
+from heapq import heappush, heappop
+
+# for map
+import sys
+input = sys.stdin.readline
+from collections import defaultdict
+V = defaultdict(int)
