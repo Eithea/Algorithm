ac882816bea58824fa211a2b250afb46bac82207
Eithea <oguraff94@gmail.com>
2022-09-29 03:50:02 +0900

    improve methods of segtree not to use recursion

--- Lib/Data structure/SegmentTree.py
+++ Lib/Data structure/SegmentTree.py
@@ -5,30 +5,29 @@ class SegmentTree :
         self.op = operator
         self.idt = idt
         self.leng = len(arr)
-        self.size = 1 << (self.leng - 1).bit_length()
+        self.size = 1 << (self.leng-1).bit_length()
         self.tree = [self.idt for _ in range(self.size*2)]
         self._createTree(arr)
 
     def _createTree(self, arr) : 
         self.tree[self.size : self.size+self.leng] = arr
-        for i in reversed(range(self.size)) :
+        for i in range(self.size-1, -1, -1) :
             self.tree[i] = self.op(self.tree[2*i], self.tree[2*i+1])
     
     def Query(self, left, right) : 
-        left += self.size
-        right += self.size+1
-
+        l = left + self.size
+        r = right + self.size + 1
         curl = self.idt
         curr = self.idt
-        while left < right:
-            if left %2:
-                curl = self.op(curl, self.tree[left])
-                left += 1
-            if right %2:
-                right -= 1
-                curr = self.op(self.tree[right], curr)
-            left //= 2
-            right //= 2
+        while l < r :
+            if l %2 :
+                curl = self.op(curl, self.tree[l])
+                l += 1
+            if r %2 :
+                r -= 1
+                curr = self.op(self.tree[r], curr)
+            l //= 2
+            r //= 2
 
         return self.op(curl, curr)
     
